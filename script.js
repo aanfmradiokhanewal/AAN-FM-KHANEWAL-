008b0b205763a026cfc4b1e5f23ffa17ecb13361
AAN FM Radio 91.60 Khanewal <jazbafmnetwork@gmail.com>
2026-07-30 11:45:46 +0500

    Update slider functionality and styles
    Refactor slider styles and functionality in script.js.

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-.slider{
-height:80vh;
-position:relative;
-overflow:hidden;
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+function showSlide(){
+
+slides.forEach(s=>s.classList.remove("active"));
+
+current++;
+
+if(current>=slides.length){
+current=0;
 }
 
-.slide{
-position:absolute;
-width:100%;
-height:100%;
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-text-align:center;
-background:linear-gradient(135deg,#8B0000,#111);
-opacity:0;
-transition:1s;
-padding:20px;
+slides[current].classList.add("active");
+
 }
 
-.slide.active{
-opacity:1;
-}
-
-.slide h1{
-font-size:55px;
-margin-bottom:20px;
-}
-
-.slide p{
-font-size:24px;
-  }
+setInterval(showSlide,4000);
